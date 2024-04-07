7d50d69999294c3b0a2b9f8c5e17c39f92c87ee7
nenadmne <cosovicnenad14@gmail.com>
2024-04-07 18:57:08 +0200

    Added logo

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -1,8 +1,10 @@
+import Logo from "../assets/logo.png";
 import "./Background.css";
 
 export default function Background({ play }) {
   return (
     <div className="background">
+      <img src={Logo} alt="logo image" className="absolute w-[15rem] top-[-3rem] z-10" />
       <div className="content">
         <div className="tunnel">
           <div className="side"></div>
