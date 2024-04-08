c9220b8787cb5eea3786a0fbfbca911d4101b31a
nenadmne <cosovicnenad14@gmail.com>
2024-04-08 13:50:24 +0200

    Scoresheet width fixed

--- frontend/src/components/ScoreSheet.jsx
+++ frontend/src/components/ScoreSheet.jsx
@@ -1,7 +1,7 @@
 export default function ScoreSheet({ playerName, playerScore }) {
   return (
     <div className="text-white flex flex-col justify-center items-center gap-2">
-      <strong className="text-3xl"> {playerName}</strong>
+      <strong className="text-3xl w-[15rem] flex justify-center items-center"> {playerName}</strong>
       <div className="text-5xl"> {playerScore} </div>
     </div>
   );
