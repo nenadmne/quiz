067f527087c8b77e9df34417d45d8c8d267c45ac
nenadmne <cosovicnenad14@gmail.com>
2024-04-10 19:53:09 +0200

    Updated button

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
 export default function ChatComponent() {
@@ -14,10 +15,21 @@ export default function ChatComponent() {
         </Button>
       </Tooltip>
       {open && (
-        <div className=" absolute w-[400px] h-[600px] bg-blueGrad left-0 bottom-[5rem] rounded-[2rem] flex flex-col justify-end p-4">
-          <div>
-            <input></input>
-            <button> Send </button>
+        <div className="absolute w-[400px] h-[600px] bg-blueGrad left-0 bottom-[5rem] rounded-[1rem] flex flex-col justify-end p-4 gap-4">
+          <div className="flex rounded-lg overflow-hidden gap-2 bg-white grow"></div>
+          <div className="flex flex-row overflow-hidden gap-2">
+            <TextField
+              className="grow p-1 bg-white p-1 rounded-lg"
+              variant="outlined"
+              size="small"
+            />
+            <Button
+              className="bg-darkPurple text-white w-[5rem] p-1"
+              variant="contained"
+              sx={{ borderRadius: "0.5rem" }}
+            >
+              Send
+            </Button>
           </div>
         </div>
       )}
