1ec3566f6eaded15efe1285b3be921b08f240edf
nenadmne <cosovicnenad14@gmail.com>
2024-04-24 19:31:34 +0200

    Added loading on leaderboard

--- frontend/src/layouts/Leaderboards.jsx
+++ frontend/src/layouts/Leaderboards.jsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Loading from "../components/Loading";
 
 export default function Leaderboards() {
   const [data, setData] = useState(null);
   const loaderData = useLoaderData();
 
   useEffect(() => {
-    setData(loaderData);
+    setTimeout(() => {
+      setData(loaderData);
+    }, 300);
   }, []);
 
-  console.log(data)
-
-  return (
-    <div className="flex flex-col justify-center items-center w-full h-full transform translate-y-[-20%]">
-      <h1>Leaderboard</h1>
+  return data !== null ? (
+    <div className="flex flex-col w-full h-full items-center p-8">
+      <>
+        <h1 className="text-[1.25rem] font-bold uppercase">Leaderboards</h1>
+        <div className="flex flex-row justify-center items-center">
+          <div>
+            <ul>
+              {data.map((item) => (
+                <li key={item.id}>{item.username}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <ul>
+              {data.map((item) => (
+                <li key={item.id}>{item.username}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    </div>
+  ) : (
+    <div className="flex flex-col justify-center items-center w-full h-full gap-4 bg-black opacity-60">
+      <Loading />
     </div>
   );
 }
