919f2a0192eb20d78749b4c2ba1d0fd8a9c3ae0b
nenadmne <cosovicnenad14@gmail.com>
2024-04-29 14:18:53 +0200

    Workaround recievedQuestion tab

--- frontend/src/layouts/RecievedQuestions.jsx
+++ frontend/src/layouts/RecievedQuestions.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckCircleIcon from "@mui/icons-material/Done";
+import CreateIcon from "@mui/icons-material/ModeEdit";
+
 export default function RecievedQuestions() {
+  const [questions, setQuestions] = useState(null);
   const data = useLoaderData();
-  console.log(data);
-  return <div>Recieved Questions</div>;
+
+  useEffect(() => {
+    if (data) {
+      setQuestions(data);
+    }
+  }, [data]);
+
+  return (
+    questions && (
+      <section className="w-[600px]">
+        <ul className="bg-greyGrad flex flex-col p-4 gap-4 rounded">
+          {questions.map((item) => (
+            <li key={item._id} className="flex flex-col gap-4 border p-2 border-black">
+              <div className="flex flex-row justify-between items-center">
+                <div className="w-[400px] flex flex-col gap-2">
+                  <div className="flex flex-row justify-between">
+                    <p>{item.question}</p>
+                    <p>{item.points}</p>
+                  </div>
+                  <div className="flex flex-row justify-between">
+                    <div className="flex flex-row gap-4">
+                      {item.answers.map((answer) => (
+                        <span>{answer}</span>
+                      ))}
+                    </div>
+                    <div>{item.correctAnswer}</div>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <CreateIcon style={{ cursor: "pointer" }} />
+                  <CheckCircleIcon style={{ cursor: "pointer" }} />
+                  <DeleteIcon style={{ cursor: "pointer" }} />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+  );
 }
 
 export const recievedQuestionsLoader = async () => {
