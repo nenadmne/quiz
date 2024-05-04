fcea5adb4faa3c0e92967af105356d7a675a0ee9
nenadmne <cosovicnenad14@gmail.com>
2024-05-04 15:43:19 +0200

    Leaderboard heading fixed

--- frontend/src/components/Leaderboard/Heading.jsx
+++ frontend/src/components/Leaderboard/Heading.jsx
@@ -3,9 +3,21 @@ import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
 export default function Heading() {
   return (
     <div className="flex flex-row gap-2 justify-center items-center">
-      <MilitaryTechIcon style={{ fontSize: "2.5rem" }} />
-      <h1 className="text-[2rem] font-bold uppercase">Leaderboard</h1>
-      <MilitaryTechIcon style={{ fontSize: "2.5rem" }} />
+      <MilitaryTechIcon
+        style={{
+          fontSize: "2.5rem",
+          filter: "drop-shadow(rgba(0, 0, 0, 0.533333) 0.1em 0.1em 0.1em)",
+        }}
+      />
+      <h1 className="text-[2rem] font-bold uppercase text-shadow">
+        Leaderboard
+      </h1>
+      <MilitaryTechIcon
+        style={{
+          fontSize: "2.5rem",
+          filter: "drop-shadow(rgba(0, 0, 0, 0.533333) 0.1em 0.1em 0.1em)",
+        }}
+      />
     </div>
   );
 }
