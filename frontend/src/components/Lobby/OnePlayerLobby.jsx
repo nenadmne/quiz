6fdab9974fc57f0e3cd3fb24a32e1a3182d5a01b
nenadmne <cosovicnenad14@gmail.com>
2024-05-07 20:44:45 +0200

    One player lobby responsive design

--- frontend/src/components/Lobby/OnePlayerLobby.jsx
+++ frontend/src/components/Lobby/OnePlayerLobby.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import LobbyBackground from "../Backgrounds/LobbyBackground";
 import vsImage from "../../assets/vs.png";
 
-const style = "text-3xl w-[400px] flex justify-center items-center p-2";
+const style =
+  "text-normal sm:text-lg md:text-2xl lg:text-3xl w-[180px] sm:w-[250px] md:w-[350px] lg:w-[400px] flex justify-center items-center p-2";
 
 export default function OnePlayerLobby({ queue, players }) {
   const [dots, setDots] = useState("");
@@ -28,8 +29,11 @@ export default function OnePlayerLobby({ queue, players }) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       <LobbyBackground />
-      <img src={vsImage} className="w-[10rem] absolute pb-[8rem] z-10" />
-      <div className="flex flex-col gap-24 justify-center items-center w-full h-full">
+      <img
+        src={vsImage}
+        className="w-[5rem] md:w-[8rem] lg:w-[9rem] absolute pb-[15rem] sm:pb-[11rem] md:pb-[5rem] lg:pb-[5rem] z-10"
+      />
+      <div className="flex flex-col gap-24 justify-center items-center w-full h-full mb-24 sm:mb-10 md:mb-[-4rem]">
         <div className="flex flex-row justify-center items-center">
           <span
             className={`${style} rounded-bl-2xl rounded-tl-2xl bg-green-500`}
@@ -43,7 +47,7 @@ export default function OnePlayerLobby({ queue, players }) {
           </span>
         </div>
         <span
-          className={`text-xl text-darkPurple px-6 py-2 bg-white rounded-xl`}
+          className={`text-normal sm:text-lg md:text-xl text-darkPurple px-6 py-2 bg-white rounded-xl`}
         >
           {`Queue time: ${queueTime(queue)}`}
         </span>
